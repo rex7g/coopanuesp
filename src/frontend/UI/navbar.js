58d8652974bdc075347bad1f44449ffb4babbb16
rex7g <renato7.rg@gmail.com>
2021-09-01 16:26:05 -0400

    fotos cooperativa

--- src/frontend/UI/navbar.js
+++ src/frontend/UI/navbar.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
+import {Link} from 'react-router-dom';
 
 export class navbar extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="navbar">
+            <div className="leftSide">
+              <img src={Logo} alt="logo"/>
+            
             </div>
+            <div className="rightSide">
+                <Link to="/home">Home </Link>
+                <Link to="/multimedia">Multimedia </Link>
+                <Link to="/contactos">Contactos </Link>
+                <Link to="/login">Acceder </Link>
+            </div>
+            
+          </div>
         )
     }
 }
